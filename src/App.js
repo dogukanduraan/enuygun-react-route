d3464d0ffee72fdff516601f8d5def94f12aaa5c
Dogukan Duran <dogukanduran@hotmail.com>
2022-06-20 16:57:44 +0300

    v1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [routes, setRoutes] = useState([]);
+  const [err, setErr] = useState(false);
+  function addRoute() {
+    setRoutes((routes) => [...routes, { isim: "", enlem: "", boylam: "" }]);
+  }
+
+  function changeField(event, index) {
+    const field = event.target.name;
+    const allRoutes = [...routes];
+    allRoutes[index][field] = event.target.value;
+
+    setRoutes(() => {
+      return allRoutes;
+    });
+  }
+
+  function isEnabledButton() {
+    let last = routes.at(routes.length - 1);
+    return (
+      routes.length < 1 ||
+      last.isim === "" ||
+      last.boylam === "" ||
+      last.enlem === ""
+    );
+  }
+
+  function onSave() {
+    const names = new Set();
+    routes.forEach((item) => {
+      names.add(item.isim);
+    });
+
+    if (names.size < routes.length) {
+      setErr(true);
+    } else {
+      setErr(false);
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="Güzergah" />
+      <button onClick={addRoute}>Yeni Güzergah Ekle</button>
+
+      {routes.map((item, index) => {
+        return (
+          <div key={index}>
+            <input
+              name="isim"
+              type="text"
+              value={routes[index].isim}
+              placeholder="Durak"
+              onChange={(event) => {
+                changeField(event, index);
+              }}
+            />
+            {err ? "Durak Adları Aynı olamaz!" : null}
+            <input
+              name="enlem"
+              type="text"
+              placeholder="Enlem"
+              value={routes[index].enlem}
+              onChange={(event) => {
+                changeField(event, index);
+              }}
+            />
+            <input
+              name="boylam"
+              type="text"
+              placeholder="Boylam"
+              value={routes[index].boylam}
+              onChange={(event) => {
+                changeField(event, index);
+              }}
+            />
+          </div>
+        );
+      })}
+      <button onClick={onSave} disabled={isEnabledButton()}>
+        Kaydet
+      </button>
     </div>
   );
 }
